21f559a9ec689460b3c1e9f381ea022997233cf3
琉見 <42371341+rumamitw01@users.noreply.github.com>
2024-03-06 09:50:55 +0800

    Folder Created

---
+++ NPTU/Database_System/0301B.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+            $a=2;
+            $b=4;
+        ?>
+        <font size="<?php echo $a ?>">
+        <p style="color:red">CBA110065</p>
+        <font size="<?php echo $b ?>">
+        <p style="color:blue">Wen-Yu Huang</p>
+    </body>
+</html>
